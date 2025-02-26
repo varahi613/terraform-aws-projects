c829ff8d1fedbc81b16de6bd71e8332cb8846237
shashidas95 <shashidas95@gmail.com>
2025-02-26 20:25:26 +0600

    images source path

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -5,12 +5,19 @@ resource "aws_s3_bucket" "tf_s3_bucket" {
     Environment = "Dev"
   }
 }
+# resource "aws_s3_object" "tf_s3_object" {
+#   bucket = aws_s3_bucket.tf_s3_bucket.bucket
+#   for_each = fileset("../public/images", "**")
+#   key    = "images/${each.key}"
+#   source = "../public/images/${each.key}"
+# }
 resource "aws_s3_object" "tf_s3_object" {
   bucket = aws_s3_bucket.tf_s3_bucket.bucket
-  for_each = fileset("../public/images", "**")
+  for_each = fileset("nodejs-mysql/public/images", "**")
   key    = "images/${each.key}"
-  source = "../public/images/${each.key}"
+  source = "nodejs-mysql/public/images/${each.key}"
 }
+
  output "bucket_name" {
      value = aws_s3_bucket.tf_s3_bucket.bucket
    }
