e3d7983494bfe357cbbb8b962ab05efd618f7a88
shashidas95 <shashidas95@gmail.com>
2025-03-04 19:57:46 +0600

    asg subnet update

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -2,7 +2,8 @@ resource "aws_autoscaling_group" "tf_auto-scaling_group" {
   desired_capacity = 2
   max_size = 5
   min_size = 1
-vpc_zone_identifier = [aws_subnet.tf_public_subnet.id]
+vpc_zone_identifier = [aws_subnet.tf_public_subnet_1.id, aws_subnet.tf_public_subnet_2.id]
+
 
   launch_template {
     id = aws_launch_template.web_lt.id
