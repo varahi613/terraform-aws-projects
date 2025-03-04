cbfe17cbd778305801106f0a73ade7a753046508
shashidas95 <shashidas95@gmail.com>
2025-03-04 18:46:17 +0600

    update route table

--- terraform/route_tables.tf
+++ terraform/route_tables.tf
@@ -1,10 +1,20 @@
-resource "aw_route_tables" "tf_public_route_table" {
-  vpc_id= aws_vpc.main.id
-  route={
-   cidr_block="10.0.0.0/0"
-   gateway_id= aws_internet_gateway.main.id
-  }
+resource "aws_internet_gateway" "tf_igw" {
+  vpc_id = aws_vpc.main.id
   tags = {
-    Name = "public-route-table"
+    Name = "sas_igw"
   }
+}
+resource "aws_route_table" "tf_route_table" {
+  vpc_id = aws_vpc.tf_vpc.id
+route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.tf_igw.id
+  }
+tags = {
+    Name = "sas_route_table"
+  }
+}
+resource "aws_route_table_association" "tf_rta_pub_sub" {
+  route_table_id = aws_route_table.tf_route_table.id
+  subnet_id      = aws_subnet.tf_public_subnet.id
 }
