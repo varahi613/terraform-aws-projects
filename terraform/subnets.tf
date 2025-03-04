64a7537a33ab534a631957fb7cd48438d5cf0f20
shashidas95 <shashidas95@gmail.com>
2025-03-04 20:13:29 +0600

    conflict subnet cidr block

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -15,7 +15,7 @@ resource "aws_subnet" "tf_public_subnet_2" {
 
   resource "aws_subnet" "private1" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"     # First private subnet
+  cidr_block              = "10.0.3.0/24"     # First private subnet
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = false            # No public IP
 
@@ -26,7 +26,7 @@ resource "aws_subnet" "tf_public_subnet_2" {
 
 resource "aws_subnet" "private2" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.3.0/24"     # Second private subnet
+  cidr_block              = "10.0.4.0/24"     # Second private subnet
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = false            # No public IP
 
