ce87d4e52a78ab2fe80bcf4ecd1bbe86f63f1808
shashidas95 <shashidas95@gmail.com>
2025-03-04 18:59:14 +0600

    up

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "tf_public_subnet" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1a"
-  map_customer_owned_ip_on_launch = true
+  map_public_ip_on_launch = true
   tags = {
     Name = "public subnet"
   }
