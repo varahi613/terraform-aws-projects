0dac48d7061bc415bebb80ac1868ae135899e7b9
varahi613 <ranjanidevops484@gmail.com>
2025-03-05 15:25:24 +0530

    Update launch_template.tf

--- terraform/launch_template.tf
+++ terraform/launch_template.tf
@@ -1,6 +1,6 @@
 resource "aws_launch_template" "web_lt" {
   name_prefix   = "web-lt"
-  image_id      = ami-07d2649d67dbe8900  # Replace with your AMI ID
+  image_id      = "ami-07d2649d67dbe8900" # Replace with your AMI ID
   instance_type = "t2.micro"
 
   tag_specifications {
